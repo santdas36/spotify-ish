9003a2eeec36bf2cf693ef5e3a883302d53c424b
santdas36 <santdas36@gmail.com>
2020-08-13 19:13:24 +0530

    commented

--- src/App.js
+++ src/App.js
@@ -25,20 +25,20 @@ function App() {
         token: _token,
       });
       
-    const discoverApi = "https://api.spotify.com/v1/views/made-for-x";
-    const headers =  {
-      method: "GET",
-      headers: {
-        'Authorization': 'Bearer ' + _token
-      }
-    };
-    
-     const getDiscoverWeekly = async () => {
-         await fetch(discoverApi, headers)
-          .then((response) => console.log('here >>>', response)) //401: unauthorized, duh
-      };
+    //const discoverApi = "https://api.spotify.com/v1/views/made-for-x";
+    //const headers =  {
+    //  method: "GET",
+    //  headers: {
+    //    'Authorization': 'Bearer ' + _token
+    //  }
+    //};
+    //
+    //const getDiscoverWeekly = async () => {
+    // await fetch(discoverApi, headers)
+    //  .then((response) => console.log('here >>>', response)) //401: unauthorized, duh
+    //};
 
-      getDiscoverWeekly();
+    //getDiscoverWeekly();
     
 
       spotifyApi.getMyTopArtists().then((response) =>
@@ -73,7 +73,7 @@ function App() {
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
         })
-      })
+      });
     }
   }, [token, dispatch]);
 
