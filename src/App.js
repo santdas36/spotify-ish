9df6955f3fc8a94f5d3f750a9056b79b72cb19cc
santdas36 <santdas36@gmail.com>
2020-08-13 18:59:11 +0530

    dj

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
       });
 
       spotifyApi.getUserPlaylists().then((playlists) => {
+        console.log(playlists);
         dispatch({
           type: "SET_PLAYLISTS",
           playlists,
