e938740cb3d7d12b56e35e947523fc40c74fbc5e
santdas36 <santdas36@gmail.com>
2020-08-13 14:53:13 +0530

    fk

--- src/SongRow.js
+++ src/SongRow.js
@@ -5,6 +5,7 @@ function SongRow({ track, playSong }) {
   console.log(track);
   return (
     <div className="songRow" onClick={() => playSong(track?.id)}>
+    { track &&
       <img className="songRow__album" src={track?.album.images[0].url} alt="" />
       <div className="songRow__info">
         <h1>{track.name}</h1>
@@ -12,6 +13,7 @@ function SongRow({ track, playSong }) {
           {track?.artists.map((artist) => artist.name).join(", ")} -{" "}{track?.album?.name}
         </p>
       </div>
+    }
     </div>
   );
 }
