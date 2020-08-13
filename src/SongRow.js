367d1a687b46713d5eb37baf1d3d02e66201bb70
santdas36 <santdas36@gmail.com>
2020-08-13 14:46:10 +0530

    f

--- src/SongRow.js
+++ src/SongRow.js
@@ -4,12 +4,12 @@ import "./SongRow.css";
 function SongRow({ track, playSong }) {
   console.log(track);
   return (
-    <div className="songRow" onClick={() => playSong(track.id)}>
-      <img className="songRow__album" src={track.album.images[0].url} alt="" />
+    <div className="songRow" onClick={() => playSong(track?.id)}>
+      <img className="songRow__album" src={track?.album.images[0].url} alt="" />
       <div className="songRow__info">
         <h1>{track.name}</h1>
         <p>
-          {track.artists?.map((artist) => artist.name).join(", ")}
+          {track?.artists.map((artist) => artist.name).join(", ")} -{" "}{track?.album?.name}
         </p>
       </div>
     </div>
